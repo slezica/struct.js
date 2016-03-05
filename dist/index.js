72741f84ab4114da97feacbed85c6ddb0c0816de
Santiago Lezica <slezica89@gmail.com>
2016-03-05 02:27:59 -0300

    Mini fix in package.json

--- dist/index.js
+++ dist/index.js
@@ -6,45 +6,54 @@ Object.defineProperty(exports, "__esModule", {
 
 var _validate = require('./validate');
 
-Object.defineProperty(exports, 'validate', {
-  enumerable: true,
-  get: function get() {
-    return _validate.validate;
-  }
-});
-
-var _factories = require('./factories');
-
-var _loop = function _loop(_key3) {
-  if (_key3 === "default") return 'continue';
-  Object.defineProperty(exports, _key3, {
-    enumerable: true,
-    get: function get() {
-      return _factories[_key3];
-    }
-  });
-};
-
-for (var _key3 in _factories) {
-  var _ret = _loop(_key3);
-
-  if (_ret === 'continue') continue;
-}
-
-var _utils = require('./utils');
-
-var _loop2 = function _loop2(_key4) {
+var _loop = function _loop(_key4) {
   if (_key4 === "default") return 'continue';
   Object.defineProperty(exports, _key4, {
     enumerable: true,
     get: function get() {
-      return _utils[_key4];
+      return _validate[_key4];
     }
   });
 };
 
-for (var _key4 in _utils) {
-  var _ret2 = _loop2(_key4);
+for (var _key4 in _validate) {
+  var _ret = _loop(_key4);
+
+  if (_ret === 'continue') continue;
+}
+
+var _factories = require('./factories');
+
+var _loop2 = function _loop2(_key5) {
+  if (_key5 === "default") return 'continue';
+  Object.defineProperty(exports, _key5, {
+    enumerable: true,
+    get: function get() {
+      return _factories[_key5];
+    }
+  });
+};
+
+for (var _key5 in _factories) {
+  var _ret2 = _loop2(_key5);
 
   if (_ret2 === 'continue') continue;
+}
+
+var _utils = require('./utils');
+
+var _loop3 = function _loop3(_key6) {
+  if (_key6 === "default") return 'continue';
+  Object.defineProperty(exports, _key6, {
+    enumerable: true,
+    get: function get() {
+      return _utils[_key6];
+    }
+  });
+};
+
+for (var _key6 in _utils) {
+  var _ret3 = _loop3(_key6);
+
+  if (_ret3 === 'continue') continue;
 }
